63d79e940c867734c3368cc124f046e2bf4d7a14
Gabe St. Angel <58677296+gabrielsaintangel@users.noreply.github.com>
2020-12-13 12:05:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import mwclient
-from mwclient import Site
 import numpy as np
 from os import path
 import pickle as pkl
+from time import sleep
 
-#get all documented snps from snpedia, writes pickle if its the first run
+#get all documented snps from snpedia, writes pickle if its the first run.
+#this data is used to extract keywords to create gene classifications
+#first run took about 4 hours to make requests for all 100k+ genes on SNPedia.
 def get_documented_snps() -> np.ndarray:
     if not path.exists("snps.pkl"):#checks if already pulled snp data
         SNP_dict = {}
         site = mwclient.Site('bots.snpedia.com', path='/')#SNPedia site path
 
         for i, page in enumerate(site.Categories['Is_a_snp']):#iterate through all SNPs on SNPedia
+            print(page.text())
             SNP_dict[page.name] = page.text(cache=False)
             if i % 1000 == 0: #there are about 100000 snps, 1000 would be 1 percent of total
-                print(i / 1000, " percent complete") #display estimated percentage
+                print(i/1000, " Percent done") #display estimated percentage
 
         file = open("snps.pkl", "wb")#save the dict as a pickle for processing in other functions
-        pkl.dump(snp_array, file,protocol=pickle.HIGHEST_PROTOCOL)
+        pkl.dump(SNP_dict, file,protocol=pkl.HIGHEST_PROTOCOL)
         file.close()
-    #if data has already been retrived, load into np array
+    #if data has already been retrived, load into dict from pickle
     else:
-        print("data already saved, loading into array")
+        print("data already saved, loading into program")
         with open('snps.pkl', 'rb') as data:
-            SNP_dict = pickle.load(data)
+            SNP_dict = pkl.load(data)
     return SNP_dict #return np array with complete SNP data
 
 
@@ -40,20 +43,16 @@ def read_your_data() -> dict:
     return genotypes
 
 #finds SNPS that are both on SNPedia and in your genome
-def get_common_SNPs(your_data, documented_SNPs) -> list:
-    your_studied_genes = []
-    for SNP in documented_SNPs:#iterate through all documented SNPs
-        if SNP in your_data: #if the key exists in your data, append it to the list
-            your_studied_genes.append(SNP)
-    return your_studied_genes
+def get_common_SNPs(your_data, documented_snps) -> list:
+    return [snp for snp in documented_snps if snp.lower() in your_data]
 
 
 
 documented_SNPs = get_documented_snps()
+
 your_data = read_your_data()
 
 
 your_studied_genes = get_common_SNPs(your_data, documented_SNPs)
-
 
 
