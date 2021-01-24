1f1ba272cd6eff872081060915592d3b97a67cf8
Gabe St. Angel <58677296+gabrielsaintangel@users.noreply.github.com>
2021-01-23 21:55:31 -0600

    Update SNPedia_crawler.py

--- scripts/SNPedia_crawler.py
+++ scripts/SNPedia_crawler.py
@@ -78,13 +78,7 @@ def crawl(rsid_urls) -> dict:
             pkl.dump(rsid_data, handle, protocol=pkl.HIGHEST_PROTOCOL)
                 
     get_data()
-
-  
-
     
-    
-
-    
-
-links = get_rsid_links()
-crawl(links)
+if __name__ == "__main__":
+    links = get_rsid_links()
+    crawl(links)
